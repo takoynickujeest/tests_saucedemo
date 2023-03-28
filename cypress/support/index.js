49f1a56cb8cf55a05ced0a61cc3d727dfa7bbc1a
Sergey Radchenko <nibus.sr@gmail.com>
2023-03-28 19:43:16 +0800

    adding /support

---
+++ cypress/support/index.js
@@ -0,0 +1,17 @@
+// // Hide fetch/XHR requests from command log
+// if (Cypress.config('hideXHRInCommandLog')) {
+//     const app = window.top;
+  
+//     if (
+//       app &&
+//       !app.document.head.querySelector('[data-hide-command-log-request]')
+//     ) {
+//       const style = app.document.createElement('style');
+//       style.innerHTML =
+//         '.command-name-request, .command-name-xhr { display: none }';
+//       style.setAttribute('data-hide-command-log-request', '');
+  
+//       app.document.head.appendChild(style);
+//     }
+//   }
+  
